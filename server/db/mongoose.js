dfa3c81f19dad813b9106508ded384f95d95fdac
William Myers <williamrmyers@gmail.com>
2018-06-03 19:21:42 -0700

    Fixed DB Bug.

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 let db = {
-  localhost: process.env.MONGODB_URI
-  // mlab: 'mongodb://williamrmyers:12345@ds239930.mlab.com:39930/todoapp'
+  localhost: process.env.MONGODB_URI,
+  mlab: 'mongodb://williamrmyers:12345@ds239930.mlab.com:39930/todoapp'
 };
 
 mongoose.connect(db.localhost || db.mlab);
